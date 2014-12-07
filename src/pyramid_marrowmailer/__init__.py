12bb20bc7aadee664c4d3e57a41638da15eeb3c6
Sandro Nardi <sandro@nardi.li>
2014-12-07 05:59:11 +0100

    tab fix

--- src/pyramid_marrowmailer/__init__.py
+++ src/pyramid_marrowmailer/__init__.py
@@ -81,7 +81,7 @@ def includeme(config):
 
     mode = '%s.mode' % prefix
     if mailer_config.get(mode) == 'direct':
-    	mailer = Mailer(mailer_config, prefix)
+        mailer = Mailer(mailer_config, prefix)
     else:
 	mailer = TransactionMailer(mailer_config, prefix)
 
