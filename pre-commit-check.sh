a5fadc79e10d42709b1ec25dc8ade8b20b53d1e5
Domen Kožar <domen@dev.si>
2018-07-11 11:21:11 +0200

    Modernize python versions on travis

--- pre-commit-check.sh
+++ pre-commit-check.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 # TODO: stash changes before doing tests: http://codeinthehole.com/writing/tips-for-using-a-git-pre-commit-hook/
 
-function handle_exit {
-    if [ $? -ne 0 ]; then
-        EXITCODE=1
-    fi
-}
+set -xe
 
 echo '====== Running tests ========='
-bin/nosetests; handle_exit
+nosetests
 
 echo '====== Running PyFlakes ======'
-bin/python setup.py flakes; handle_exit
+python setup.py flakes
 
 echo '====== Running pep8 =========='
-bin/pep8 src/pyramid_marrowmailer; handle_exit
-bin/pep8 *.py; handle_exit
+pep8 src/pyramid_marrowmailer
+pep8 *.py
